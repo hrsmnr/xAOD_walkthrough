dabfb14bf428bbb85da0d481fe750073ae5a544a
hrsmnr <hrsmnr@users.noreply.github.com>
2015-09-08 13:49:13 +0900

    Added missing directory(i.e. .gitkeep)

--- MyAnalysis/Root/EventSelector.cxx
+++ MyAnalysis/Root/EventSelector.cxx
@@ -1063,14 +1063,14 @@ void EventSelector::Set3S4BZvetoBvetoMet()
 /*--------------------------------------------------------------------------------*/
 void EventSelector::SetGT1S3B()
 {
-  SetBveto(); // require b veto
+  //  SetBveto(); // require b veto
   m_nLepMin = 1;
   m_nLepMax = 3;
   m_nBaseLepMin = m_nBaseLepMax = 3;
   m_selSFOS = true;
   m_applyTrig = false;
   m_1stBaseIsSignal = true;
-  m_baseLepEtaMax = 1.5; // require all baseline lepton's |eta| < 1.5
+  //  m_baseLepEtaMax = 1.5; // require all baseline lepton's |eta| < 1.5
   return;
 }
 /*--------------------------------------------------------------------------------*/
